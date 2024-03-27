acbdbfb79fb2f5d6ec69c1114d8b772b3f280865
Tyler.Darsey@coxautoinc.com <Tyler.Darsey@coxautoinc.com>
2024-03-27 14:29:56 -0500

    Refix, making backend manually

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,32 +1,4 @@
-locals {
-    s3_bucket = "terraform-${var.project_name}-${var.region}"
-    dynamo_table = "terraform-${var.project_name}-${var.region}"
-}
-
 terraform {
   backend "s3" {
-    # These are plugged in through our GitHub Actions
-
-    # encrypt = true
-    # bucket = local.s3_bucket
-    # dynamodb_table = local.dynamo_table
-    # key    = "terraform.tfstate"
-    # region = var.region
-  }
-}
-
-resource "aws_s3_bucket" "tf_course" {
-    bucket = local.s3_bucket
-}
-
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name = "terraform-state-lock-dynamo"
-  hash_key = "LockID"
-  read_capacity = 20
-  write_capacity = 20
- 
-  attribute {
-    name = "LockID"
-    type = "S"
   }
 }
